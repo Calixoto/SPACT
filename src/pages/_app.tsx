2d99348b9eec6398a01aa1e764594fe27c9c04ca
calixototo <lucascalixto@mobister.com.br>
2022-10-23 19:24:06 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
