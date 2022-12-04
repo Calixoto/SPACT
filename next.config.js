783bd4431ef97c0a7f49a727b0af4d105539c6b3
Lucas Calixto <lucas70matheus@gmail.com>
2022-12-04 00:08:51 -0300

    remove exposed api key

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compress: true,
   images: {
     domains: ["i.ytimg.com", "yt3.ggpht.com"],
+  },
+  env: {
+    YOUTUBE_API_KEY: "AIzaSyAog7LHSwVP-AiDA7PxMiCEkAsj5IfyduI",
   },
   experimental: {
     appDir: true,
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+const key = process.env.YOUTUBE_API_KEY;
+
 export const api = axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 4,
-    key: "AIzaSyAog7LHSwVP-AiDA7PxMiCEkAsj5IfyduI",
+    key,
   },
   headers: {},
 });
