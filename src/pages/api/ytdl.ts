18f1342bbcfb06bff13eb1433cbc7c39b52929da
VictorHAS <victorhermesalves@gmail.com>
2022-12-10 18:46:37 -0300

    feat: adds MQTT

--- src/pages/api/ytdl.ts
+++ src/pages/api/ytdl.ts
@@ -4,6 +4,8 @@ import ytdl from "ytdl-core";
 import aws from "./creatingAws";
 import { generateRandomString } from "../../utils/generateRandomString";
 
+import { connect } from "mqtt";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
@@ -12,7 +14,7 @@ export default async function handler(
     try {
       const url = req.body.url;
       const format = req.body.type;
-
+      const client = connect("mqtt://broker.hivemq.com:1883");
       if (!ytdl.validateID(url) && !ytdl.validateURL(url)) {
         return res
           .status(400)
@@ -23,6 +25,7 @@ export default async function handler(
         const file = ytdl(url, { format, filter: "audioonly" });
         const Key = `${generateRandomString(8)}.mp3`;
         const urlSigned = await aws(file, Key);
+        client.publish("spact/download", urlSigned as string);
         return res.send({ success: true, data: "Upload done", urlSigned });
       }
     } catch (err) {
